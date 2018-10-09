8c42bbeb82679afbcb2860dbc73402808cb398d9
Shubham Awasthi <aw.shubh@gmail.com>
2018-10-09 11:08:27 +0530

    Added gitignore

--- config/config.js
+++ config/config.js
@@ -2,8 +2,6 @@ if(!process.env.HEROKU)
     module.exports.keys=require('./keys')
 else{
     module.exports.keys={
-        email: process.env.email,
-        password: process.env.password,
-        db: process.env.db
+        
     }
 }
